dd67f53872afc56a109733425f5b14f4f5bca06f
mohsin1022 <bcsf13m030@gmail.com>
2017-05-19 11:50:39 +0500

    db update

--- Din Media Group Job Portal/Models/tb_employer_registration_data.cs
+++ Din Media Group Job Portal/Models/tb_employer_registration_data.cs
@@ -16,7 +16,6 @@ namespace Din_Media_Group_Job_Portal.Models
     {
         public int id { get; set; }
         public string email { get; set; }
-
         public string first_name { get; set; }
         public string last_name { get; set; }
         public string company_name { get; set; }
